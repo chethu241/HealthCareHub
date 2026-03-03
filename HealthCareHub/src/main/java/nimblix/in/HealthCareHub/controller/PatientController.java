85c721cae814bca568a4f4be22485f9c88a6f4f5
chethu241 <chethanac169@gmail.com>
2026-03-03 18:54:54 +0530

    Implement Patient Registration feature

--- HealthCareHub/src/main/java/nimblix/in/HealthCareHub/controller/PatientController.java
+++ HealthCareHub/src/main/java/nimblix/in/HealthCareHub/controller/PatientController.java
@@ -1,6 +1,13 @@
 package nimblix.in.HealthCareHub.controller;
 
 import lombok.RequiredArgsConstructor;
+import nimblix.in.HealthCareHub.request.PatientRegistrationRequest;
+import nimblix.in.HealthCareHub.response.PatientRegistrationResponse;
+import nimblix.in.HealthCareHub.service.PatientService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/patient")
 @RequiredArgsConstructor
 public class PatientController {
+
+    private final PatientService patientService; // injected via constructor
+
+    @PostMapping("/register")
+    public ResponseEntity<PatientRegistrationResponse> register(@RequestBody PatientRegistrationRequest request) {
+        PatientRegistrationResponse response = patientService.registerPatient(request);
+
+        if (!response.isSuccess()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+
+        return ResponseEntity.ok(response);
+    }
+
 }
--- HealthCareHub/src/main/java/nimblix/in/HealthCareHub/service/PatientService.java
+++ HealthCareHub/src/main/java/nimblix/in/HealthCareHub/service/PatientService.java
@@ -1,4 +1,11 @@
 package nimblix.in.HealthCareHub.service;
 
+
+import nimblix.in.HealthCareHub.request.PatientRegistrationRequest;
+import nimblix.in.HealthCareHub.response.PatientRegistrationResponse;
+
 public interface PatientService {
+    // existing methods ...
+
+    PatientRegistrationResponse registerPatient(PatientRegistrationRequest request);
 }
