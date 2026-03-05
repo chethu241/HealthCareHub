0d82f72df8dc52c3514856cc29c37b06078763af
chethu241 <chethanac169@gmail.com>
2026-03-05 17:31:11 +0530

    Implement patient search API

--- HealthCareHub/src/main/java/nimblix/in/HealthCareHub/controller/PatientController.java
+++ HealthCareHub/src/main/java/nimblix/in/HealthCareHub/controller/PatientController.java
@@ -1,11 +1,28 @@
 package nimblix.in.HealthCareHub.controller;
 
 import lombok.RequiredArgsConstructor;
+import nimblix.in.HealthCareHub.response.PatientSearchResponse;
+import nimblix.in.HealthCareHub.service.PatientService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/patient")
 @RequiredArgsConstructor
 public class PatientController {
+
+    private final PatientService patientService;
+
+    @GetMapping("/search")
+    public List<PatientSearchResponse> searchPatients(
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "phone", required = false) String phone,
+            @RequestParam(value = "bloodGroup", required = false) String bloodGroup
+    ) {
+        return patientService.searchPatients(name, phone, bloodGroup);
+    }
 }
